4b3bacdc75e04514395cbcc431b389d89ffc04ce
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2023-03-30 18:55:10 -0700

    use templating in `.gomplateignore`
    Signed-off-by: Mathew Wicks <thesuperzapper@users.noreply.github.com>

--- run_generator.sh
+++ run_generator.sh
@@ -5,19 +5,35 @@ set -euo pipefail
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
 
+GENERATOR_SOURCE_PATH="./generator"
+GENERATOR_OUTPUT_PATH="./GENERATOR_OUTPUT"
+CUSTOM_VALUES_PATH="./values.yaml"
+
 # clean the generator output directory
-rm -rf ./GENERATOR_OUTPUT/
+rm -rf "$GENERATOR_OUTPUT_PATH"
 
 # suppress the generation of empty files
 export GOMPLATE_SUPPRESS_EMPTY=true
 
-# populate generator output directory
+# PHASE 1: render our `.gomplateignore_template` files
 gomplate \
-  --input-dir='./generator/templates' \
-  --output-dir='./GENERATOR_OUTPUT' \
-  --left-delim '{{<' \
-  --right-delim '>}}' \
-  --datasource 'Values_default=./generator/default_values.yaml' \
-  --datasource 'Values_custom=./values.yaml' \
-  --context 'Values=merge:Values_custom|Values_default' \
-  --template './generator/helpers/'
+  --input-dir="$GENERATOR_SOURCE_PATH/templates" \
+  --output-map="$GENERATOR_SOURCE_PATH/templates/{{< .in | strings.ReplaceAll \".gomplateignore_template\" \".gomplateignore\" >}}" \
+  --include ".gomplateignore_template" \
+  --left-delim "{{<" \
+  --right-delim ">}}" \
+  --datasource "Values_default=$GENERATOR_SOURCE_PATH/default_values.yaml" \
+  --datasource "Values_custom=$CUSTOM_VALUES_PATH" \
+  --context "Values=merge:Values_custom|Values_default" \
+  --template "$GENERATOR_SOURCE_PATH/helpers/"
+
+# PHASE 2: populate generator output directory
+gomplate \
+  --input-dir="$GENERATOR_SOURCE_PATH/templates" \
+  --output-dir="$GENERATOR_OUTPUT_PATH" \
+  --left-delim "{{<" \
+  --right-delim ">}}" \
+  --datasource "Values_default=$GENERATOR_SOURCE_PATH/default_values.yaml" \
+  --datasource "Values_custom=$CUSTOM_VALUES_PATH" \
+  --context "Values=merge:Values_custom|Values_default" \
+  --template "$GENERATOR_SOURCE_PATH/helpers/"
