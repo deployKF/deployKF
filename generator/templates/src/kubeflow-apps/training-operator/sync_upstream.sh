023a04f028a5e3b5eb0bcd2b67acf6b6963b09c3
Mathew Wicks <thesuperzapper@users.noreply.github.com>
2023-04-16 15:01:30 -0700

    localize upstream resources (training-operator)
    Signed-off-by: Mathew Wicks <thesuperzapper@users.noreply.github.com>

---
+++ generator/templates/src/kubeflow-apps/training-operator/sync_upstream.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$THIS_SCRIPT_PATH"
+
+# upstream configs
+UPSTREAM_REPO="github.com/kubeflow/manifests"
+UPSTREAM_PATH="apps/training-operator/upstream/overlays/kubeflow"
+UPSTREAM_REF="f038f81df39f3606c14ad52e06f60f3d9c5bddfb" # v1.6.1
+
+# output configs
+OUTPUT_PATH="./upstream"
+
+# clean the generator output directory
+rm -rf "$OUTPUT_PATH"
+
+# localize the upstream resources with kustomize
+# - https://kubectl.docs.kubernetes.io/references/kustomize/cmd/localize/
+kustomize localize "${UPSTREAM_REPO}/${UPSTREAM_PATH}?ref=${UPSTREAM_REF}" "$OUTPUT_PATH"
