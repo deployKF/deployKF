b1e5119593e32ae4e4249dd07d2ffadcf6aa8d9a
Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>
2024-04-11 19:43:26 -0700

    docs: add argocd helm example for plugin
    Signed-off-by: Mathew Wicks <5735406+thesuperzapper@users.noreply.github.com>

---
+++ argocd-plugin/argocd-helm/helm_install_argocd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$THIS_SCRIPT_PATH"
+
+ARGOCD_NAMESPACE="argocd"
+HELM_RELEASE_NAME="argocd"
+
+# add the argocd helm repo
+helm repo add "argo" https://argoproj.github.io/argo-helm
+
+# install argocd with helm
+# NOTE: the chart and argocd versions are NOT aligned
+#       https://artifacthub.io/packages/helm/argo/argo-cd
+helm install "$HELM_RELEASE_NAME" "argo/argo-cd" \
+  --namespace "$ARGOCD_NAMESPACE" \
+  --create-namespace \
+  --version "6.7.11" \
+  --values ./values.yaml
---
+++ argocd-plugin/argocd-helm/helm_upgrade_argocd.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$THIS_SCRIPT_PATH"
+
+ARGOCD_NAMESPACE="argocd"
+HELM_RELEASE_NAME="argocd"
+
+# add the argocd helm repo
+helm repo add "argo" https://argoproj.github.io/argo-helm
+
+# install argocd with helm
+helm upgrade "$HELM_RELEASE_NAME" "argo/argo-cd" \
+  --namespace "$ARGOCD_NAMESPACE" \
+  --version "6.7.11" \
+  --values ./values.yaml
